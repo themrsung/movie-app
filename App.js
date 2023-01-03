06d55e92f9a88cc55761c5509989dd21ec2ae079
MJ Sung <themrsung@gmail.com>
2023-01-03 12:46:13 +0900

    removed styles

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from "expo-status-bar"
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View, ScrollView, Image } from "react-native"
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto" />
-        </View>
+        <ScrollView>
+            <View>
+                <Image />
+                <View></View>
+            </View>
+            <ScrollView>
+                <View></View>
+                <View></View>
+            </ScrollView>
+        </ScrollView>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center"
-    }
-})
